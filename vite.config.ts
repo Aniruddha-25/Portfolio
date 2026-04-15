b2fd966e128cfcd53ba29c504b25228ee2741393
Aniruddha Salvankar <aniruddha.salvankar2523@gmail.com>
2026-04-15 09:16:02 +0530

    Updated the Page to work with Portfolio Github Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// Set base to '/Portfolio/' for GitHub Pages
 export default defineConfig({
+  base: '/Portfolio/',
   plugins: [react()],
 })
